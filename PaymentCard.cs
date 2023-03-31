bf17465470f534b41f1dccb84138c2d3bf2240aa
HannaMahilevich <ann.mogilevich@gmail.com>
2023-03-31 14:40:42 +0200

    Second HW(constructor and private info)

--- PaymentCard.cs
+++ PaymentCard.cs
@@ -1,7 +1,37 @@
 public class PaymentCard
 {
-    public string CardNumber;
-    public string UserName;
-    public string ExpirationDate;
-    public short CVV;
+    private string CardNumber;
+    private string UserName;
+    private string ExpirationDate;
+    private short CVV;
+
+    public PaymentCard (string cardNumber, string userName, string expirationDate, short cvv)
+    {
+        CardNumber = cardNumber;
+        UserName = userName;
+        ExpirationDate = expirationDate;
+        CVV =cvv;
+    }
+
+    // public string GetCardNumber()
+    // {
+    //     return CardNumber;
+    // }
+    // public string GetUserName()
+    // {
+    //     return UserName;
+    // }
+    // public string GetExpirationDate()
+    // {
+    //     return ExpirationDate;
+    // }
+    // public short GetCVV()
+    // {
+    //     return CVV;
+    // }
+
+    public string GetFullInfo()
+    {
+        return String.Format("Card number: {0}, User's name and surname: {1}, expiration date {2}, CVV: {3}", CardNumber, UserName, ExpirationDate, CVV);
+    }
 }
--- Program.cs
+++ Program.cs
@@ -2,42 +2,19 @@
 {
     foreach (PaymentCard paymentCard in cards)
     {
-        Console.WriteLine("Card number: {0}, User's name and surname: {1}, expiration date {2}, CVV: {3}", paymentCard.CardNumber,
-                                                                                                            paymentCard.UserName,
-                                                                                                            paymentCard.ExpirationDate,
-                                                                                                            paymentCard.CVV);
+        Console.WriteLine(paymentCard.GetFullInfo());
     }
 }
 
-PaymentCard paymentCard1 = new PaymentCard();
-paymentCard1.CardNumber = "0000 4141 1010 0180";
-paymentCard1.UserName = "Vasya Ivanov";
-paymentCard1.ExpirationDate = "03/25";
-paymentCard1.CVV = 308;
+PaymentCard paymentCard1 = new PaymentCard("0000 4141 1010 0180", "Vasya Ivanov", "03/25", 308);
 
-PaymentCard paymentCard2 = new PaymentCard();
-paymentCard2.CardNumber = "0220 1342 0000 0909";
-paymentCard1.UserName = "Ivan Vasiliev";
-paymentCard2.ExpirationDate = "04/26";
-paymentCard2.CVV = 122;
+PaymentCard paymentCard2 = new PaymentCard("0220 1342 0000 0909", "Ivan Vasiliev", "04/26", 122);
 
-PaymentCard paymentCard3 = new PaymentCard();
-paymentCard3.CardNumber = "1234 3894 0000 0000";
-paymentCard3.UserName = "Vanya Vanyo";
-paymentCard3.ExpirationDate = "09/25";
-paymentCard3.CVV = 984;
+PaymentCard paymentCard3 = new PaymentCard("1234 3894 0000 0000", "Vanya Vanyo", "09/25", 984);
 
-PaymentCard paymentCard4 = new PaymentCard();
-paymentCard4.CardNumber = "5609 0000 3450 3456";
-paymentCard4.UserName = "Vasiliy Petrov";
-paymentCard4.ExpirationDate = "07/24";
-paymentCard4.CVV = 773;
+PaymentCard paymentCard4 = new PaymentCard("5609 0000 3450 3456", "Vasiliy Petrov", "07/24", 773);
 
-PaymentCard paymentCard5 = new PaymentCard();
-paymentCard5.CardNumber = "0000 3330 2234 0894 3545";
-paymentCard5.UserName = "Petr Sharp";
-paymentCard5.ExpirationDate = "08/27";
-paymentCard5.CVV = 708;
+PaymentCard paymentCard5 = new PaymentCard("0000 3330 2234 0894 3545", "Petr Sharp", "08/27", 708);
 
 PaymentCard[] cards = new PaymentCard[] {paymentCard1, paymentCard2, paymentCard3, paymentCard4, paymentCard5};
 
